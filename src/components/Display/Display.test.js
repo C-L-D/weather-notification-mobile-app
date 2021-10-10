4901bb1bd4ede9d99636cac8e32b3545044ba4ed
C-L-D <clairedoswell1995@gmail.com>
2021-10-10 10:52:34 +0100

    Added tests for Display and Notification components

--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -1,5 +1,32 @@
 // Tests for Display component
+// import component
+// import image
+// import jest-dom native
 // Display component should:
 // render an image.
+// image has alt text
 // render given text in bold
 // render given text
+
+import Display from "./";
+import image from "../../../assets/cloud.png";
+import "@testing-library/jest-native/extend-expect";
+
+describe("The display component should:", () => {
+  test("render an image", () => {
+    const { getByTestId } = render(<Display src={image} />);
+    expect(getByTestId("image")).toBeInTheDocument();
+  });
+  test("image has alt text", () => {
+    const { getByTestId } = render(<Display src={image} alt="alt text" />);
+    expect(getByTestId("image")).toHaveProp({ prop: "alt", value: "alt text" });
+  });
+  test("render correct text in bold", () => {
+    const { getByText } = render(<Display text="Pressure" />);
+    expect(getByText("Pressure")).toBeInTheDocument();
+  });
+  test("render data text", () => {
+    const { getByText } = render(<Display data="1234" />);
+    expect(getByText("1234")).toBeInTheDocument();
+  });
+});
---
+++ src/components/Notification/Notification.test.js
@@ -0,0 +1,66 @@
+// Tests for Notification component
+// import component
+// import image
+// import jest-dom native
+// The notification component should:
+// send a push notification
+// with given text in bold
+// with given text
+// with a green yes button
+// with a red no button
+
+import Notification from "./";
+import image from "../../../assets/cloud.png";
+import "@testing-library/jest-native/extend-expect";
+
+describe("The notification component should:", () => {
+  test("produce a push notification", () => {
+    const { getByTestId } = render(<Notification />);
+    expect(getByTestId("notification")).toBeInTheDocument();
+  });
+  test("render correct text in bold", () => {
+    const { getByText } = render(<Notification data="1234" />);
+    expect(getByText("1234")).toBeInTheDocument();
+  });
+  test("render data text", () => {
+    const { getByText } = render(<Notification text="Pressure is above:" />);
+    expect(getByText("Pressure is above:")).toBeInTheDocument();
+  });
+  describe("Yes button should:", () => {
+    test("be in the document", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("notification")).toContainElement(
+        getByTestId("yesButton")
+      );
+    });
+    test("be green", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("yesButton")).toHaveStyle({
+        style: { backgroundColor: "green" },
+      });
+    });
+    test("have an onClick", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("yesButton")).toHaveProp({ prop: "onClick" });
+    });
+  });
+
+  describe("No button should:", () => {
+    test("be in the document", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("notification")).toContainElement(
+        getByTestId("noButton")
+      );
+    });
+    test("be red", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("noButton")).toHaveStyle({
+        style: { backgroundColor: "red" },
+      });
+    });
+    test("have an onClick", () => {
+      const { getByTestId } = render(<Notification />);
+      expect(getByTestId("noButton")).toHaveProp({ prop: "onClick" });
+    });
+  });
+});
